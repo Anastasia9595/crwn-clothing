c4d75ffa2b1db20b0f8ebfab373ce98b02a1dedb
anastasia <Anastasia9595@googlemail.com>
2022-07-28 13:53:29 +0200

    Add Navigation Bar

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from 'react';
+
+import {Outlet, Link} from 'react-router-dom';
+
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
+const Navigation = () => {
+    return(
+      <Fragment>
+        <div className='navigation'>
+            <Link className='navigation_logo' to='/'><CrwnLogo className='logo'/></Link>
+            <div className='navigation_menu'>
+                <Link className='nav-link' to='/shop'>Shop</Link>
+            
+            </div>      
+        </div>
+        <Outlet />
+      
+      </Fragment>
+    );
+  }
+
+  export default Navigation;
