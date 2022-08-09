80686fa57bdf6602eeb4a6214c18492daaeca7ae
anastasia <Anastasia9595@googlemail.com>
2022-08-09 20:53:02 +0200

    adding cart context and shopping icon

--- src/index.js
+++ src/index.js
@@ -3,15 +3,21 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductProvider } from './contexts/product.context';
+import {CartProvider} from './contexts/cart.context'
 import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
+      <UserProvider>
+        <ProductProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </ProductProvider>
+      </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,7 +4,11 @@ import {Outlet, Link} from 'react-router-dom';
 
 import {ReactComponent as CrwnLogo} from '../../assets/crown.svg';
 
+import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropDown from '../../components/cart-dropdown/cart-dropdown.components';
+
 import { UserContext } from '../../contexts/user.context';
+import {CartContext} from '../../contexts/cart.context'
 
 import {signOutUser} from '../../utils/firebase/firebase.utils'
 
@@ -13,7 +17,7 @@ import './navigation.styles.scss';
 const Navigation = () => {
 
     const {currentUser} = useContext(UserContext);
-   
+    const {isCartOpen} = useContext(CartContext);
 
     return(
       <Fragment>
@@ -26,8 +30,11 @@ const Navigation = () => {
                   currentUser ? (<span className='nav-link'onClick={signOutUser}> SIGN OUT</span>) : 
                   (<Link className='nav-link' to='/auth'>SIGN IN</Link>)
                 }
+
+                <CartIcon/>
                 
-            </div>      
+            </div>
+            {isCartOpen && <CartDropDown/> }     
         </div>
         <Outlet />
       
